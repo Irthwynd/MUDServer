1a67828c04c17378b95c7651de0f2480bce9298b
Tony Crosco <irthwynd@gmail.com>
2025-04-11 17:15:06 -0400

    Minor Changes

--- obj/Debug/net9.0/MUDServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/MUDServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MUDServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d74e06ade499f851c3e27b455ed69bce5147056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c0a3fdd95a1ae8fca233fe639245fd2194fa5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MUDServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MUDServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
